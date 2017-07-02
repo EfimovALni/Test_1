aaef7e68e9f92b31c315a33213c6401eab23cabf
Alexander Efimov <alniefimov@gmail.com>
2017-07-02 20:53:10 +0200

    Third commit. Add some loops...

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,19 @@
  * Created by alex on 2.7.17.
  */
 public class Test {
+
     public static void main(String[] args) {
-        System.out.printf("First commit!\n");
-        System.err.println("Second commit! Like ERROR");
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Number of you dog: " + i);
+        }
+
+        age(20);
+    }
+
+    private static void age(int i) {
+        for (int j = 0; j < i; j+=2) {
+            System.out.print("._-");
+        }
     }
 }
