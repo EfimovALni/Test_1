7e7b95b7c4ef6cfaf9d5b9b0a0bc17a12e135077
Alexander Efimov <AlNiEfimov@gmail.com>
2017-07-03 10:53:58 +0200

    Add class morning

--- src/Test.java
+++ src/Test.java
@@ -2,19 +2,28 @@
  * Created by alex on 2.7.17.
  */
 public class Test {
+    private static boolean flag;
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 3; i++){
             System.out.println("Number of you dog: " + i);
         }
 
         age(20);
+        morning(5);
     }
 
     private static void age(int i) {
         for (int j = 0; j < i; j+=2) {
             System.out.print("_|-");
+        }
+        flag = true;
+    }
+
+    private static void morning(int i) {
+        if (flag == true) {
+            System.err.println("I'm happy & successful");
         }
     }
 }
