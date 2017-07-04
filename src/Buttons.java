3104a5ae409d24c76ace0f79d326fcc0b3f0a67b
Alexander Efimov <AlNiEfimov@gmail.com>
2017-07-04 16:26:55 +0200

    Добавлена возможность рассчитывать ширинуприложенияпо ширене экрана!

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,10 +1,20 @@
+/*
+import javax.swing.*;
+import java.awt.*;
+
+*/
 /**
- * Created by alex on 4.7.17.
- */
+ * Created by alex on 3.7.17.
+ *//*
+
 public class Buttons {
-    public Buttons() {
-        System.err.println("\nButtons");
+    public JButton one;
+
+    public void buttons() {
+        one = new JButton("1");
+        one.setBounds(50,50,50,50);
+//        add(one);
 
     }
-
 }
+*/
--- src/Test.java
+++ src/Test.java
@@ -1,40 +1,64 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by alex on 2.7.17.
  */
 public class Test extends JFrame{
-    private static boolean flag;
+    public JButton one;
+    public JButton two;
+
+
+    public double width;
+    public double height;
 
     public static void main(String[] args) {
-
-        Operations operations = new Operations();
-
-        for (int i = 0; i < 3; i++){
-            System.out.println("Number of you dog: " + i);
-        }
-
-        Age age = new Age();
-        morning(operations.left);
 
         new Test();
     }
 
-    private static void morning(int left) {
-        System.err.println("\nI'm happy & successful");
-    }
-
     public Test() {
         super ("Test");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        width = screenSize.width;
+        height = screenSize.height;
+
+//        Buttons buttons = new Buttons();
+        buttons();
         GUI(this);
-        Buttons buttons = new Buttons();
+
 
     }
     private void GUI(Test app) {
-        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        app.setSize(200,300);
-        app.setLocation(500, 600);
-        app.setVisible(true);
 
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setSize((int) width / 5,(int) height / 3);
+        app.setLayout(null);
+        app.setLocation(400, 300);
+        app.setVisible(true);
     }
+
+    public void buttons(){
+
+        one = new JButton("1");
+        one.setBounds(20,20,60,60);
+        one.setFont(new Font("Comic Sanc", Font.PLAIN, 20));
+        one.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println("one");
+                System.out.println((int) width / 5);
+                System.out.println((int) height / 3);
+            }
+        });
+        add(one);
+
+        two = new JButton("2");
+        two.setBounds(50, 50, Test.getWindows().length / 10 , Test.getFrames().length / 4);
+        two.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(two);
+    }
+
 }
